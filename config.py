cdad2300e6e3a5c74114ea97b634669e2fa294d3
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-06-01 04:42:05 -0500

    Filter US_reopen data

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-ROOT_PATH = "/Users/mdrafiqulrabin/Desktop/COVID19-Twitter-Sentiment/"
+ROOT_PATH = "/Users/mdrafiqulrabin/Desktop/COVID19/COVID19-Twitter-Reopen/"
 DATA_PATH = ROOT_PATH + "Data/"
 
 RX_SPACE   = r'\s+'
@@ -9,7 +9,66 @@ RX_HASHTAG = r'#(\w+)'
 RX_MENTION = r'@(\w+)'
 RX_ONLY_AB = r'[^A-Za-z]'
 
-ALL_DATA = DATA_PATH + "covid19.csv"
+ALL_DATA = DATA_PATH + "covid19_all.csv"
 US_DATA = DATA_PATH + "covid19_us.csv"
 US_REOPEN_DATA  = DATA_PATH + "covid19_us_reopen.csv"
 
+US_STATES = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+    'United States of America': 'USA'
+}
